9aa945279451ff6201c9b6db8ca2b99042c73163
jojohahana <yohana.chriezt@gmail.com>
2023-07-10 11:45:41 +0700

    add App Bar, Logo & Font

--- lib/feature/detail_page.dart
+++ lib/feature/detail_page.dart
@@ -5,6 +5,48 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        toolbarHeight: 101,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/Logo1.png',
+            ),
+          ],
+        ),
+        backgroundColor: const Color(0xffE3F0FE),
+      ),
+      body: const Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              'Total Cost',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text('Total'),
+            Text('Weight (gram)'),
+            Text(
+              'From',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text('Province'),
+            Text('City'),
+            Text(
+              'From',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text('Province'),
+            Text('City')
+          ],
+        ),
+      ),
+    );
   }
 }
